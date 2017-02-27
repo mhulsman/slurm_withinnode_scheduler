c5901840dad0d8c173c494cf7daca0f47a3e081d
Marc Hulsman <hulsmanm@tcn1061.cartesius.surfsara.nl>
2017-02-27 07:57:14 +0100

    Latest

--- xslurm_shared.py
+++ xslurm_shared.py
@@ -2,7 +2,7 @@ import xmlrpclib
 import httplib
 import socket
 from dns import resolver, reversename
-
+import time
 
 #COMMANDS
 NOOP=0
@@ -10,7 +10,7 @@ STOP=1
 DIE=2
 CANCEL=3
 REREGISTER=4
-
+DEASSIGN=5
 
 #MODES
 RUNNING = 1
@@ -41,18 +41,35 @@ port = 38763
 address = '127.0.0.1'
 job_url = 'http://' + address + ':' + str(port + 1)
 
+cache_hostname = None
 def get_hostname():
     #get IP address
-    socket.setdefaulttimeout(30)
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM); 
-    s.connect(('google.com', 0)); 
-    myip = s.getsockname()[0]
+    global cache_hostname
+    if not cache_hostname is None:
+        return cache_hostname
+    adresses = ['google.com', 'nu.nl', 'tweakers.net']
+    
+    while adresses:
+        try:
+            socket.setdefaulttimeout(30)
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM); 
+            s.connect((adresses[0], 0)); 
+            myip = s.getsockname()[0]
+            s.close()
+            break
+        except:
+            adresses = adresses[1:]
+
 
     try:
         addr=reversename.from_address(myip)
         myip = str(resolver.query(addr,"PTR")[0])[:-1]
     except Exception,e:
         pass
-
+    
+    cache_hostname = myip
     return myip
 
+def short_name(name):
+    return name.split(".")[0].split('-')[0]
+    
